4af06899e565102bea13556f503bcdfed393d312
Tarun Gupta J <tjguptat1rs@gmail.com>
2020-06-14 22:43:46 +0530

    Api calls only for Headlines dont require content

--- searchbytopic.py
+++ searchbytopic.py
@@ -1,7 +1,7 @@
 import requests
 from apikeyconstants import *
 
-url = ('http://newsapi.org/v2/everything?'
+url = ('http://newsapi.org/v2/top-headlines?'
     'q=Apple&'
     'language=en&'
     'from=2020-05-14&'
