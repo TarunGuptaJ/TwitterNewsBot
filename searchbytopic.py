1259323a24c496f7ca181c393bcdf26125e2425a
Tarun Gupta J <tjguptat1rs@gmail.com>
2020-06-15 22:54:14 +0530

    Accessing all parameters from the News API

--- searchbytopic.py
+++ searchbytopic.py
@@ -1,12 +1,30 @@
 import requests
-from apikeyconstants import *
+import random
+from outsideconstants import *
 
 url = ('http://newsapi.org/v2/top-headlines?'
-    'q=Apple&'
+    'q=apple&'
     'language=en&'
-    'from=2020-05-14&'
+    'from=2020-05-15&'
     'sortBy=popularity&'
     'apiKey='+str(NewsApiKeyMine))
 response = requests.get(url).json()
 
-print(response)
+totalarticles = len(response['articles'])
+chosearticle = random.randint(0,totalarticles-1)
+
+for i in range(totalarticles):
+    print(i)
+    print(response['articles'][i])
+    print(response['articles'][i]['description'])
+    print(len(response['articles'][i]['description']))
+    print()
+    # Len of >
+# print(chosearticle)
+# print(response['articles'][chosearticle]['description'])
+
+# print(0)
+# print(response['articles'][0]['description'])
+
+# print(totalarticles-1)
+# print(response['articles'][totalarticles-1]['description'])
